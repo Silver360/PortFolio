bb045513c1ed720d12e64fc8cf01791a70277e84
Ren <Silverado360@gmail.com>
2015-08-30 22:23:52 +0200

    Add About and Experience

--- portfolio/client/lib/directives.js
+++ portfolio/client/lib/directives.js
@@ -16,6 +16,12 @@ angular.module('portfolio').directive('scrollSpy', function (){
             }, 1000);
         })
 
+        $('.experience').click(function(){
+            element.stop().animate({
+                scrollTop: $("#experience").offset().top - 71
+            }, 1000);
+        })
+
     }
 
 
@@ -27,3 +33,60 @@ angular.module('portfolio').directive('scrollSpy', function (){
     };
 
 });
+
+angular.module('portfolio').directive('resizeFoto', function (){
+
+    function link(scope, element, attrs) {
+
+        var highEle = element.height();
+        element.find('.img').height(highEle);
+
+    }
+
+
+    return{
+        restrict: 'A',
+        link: link
+
+
+    };
+
+});
+
+
+//angular.module('portfolio').directive('animationScrolling', function() {
+//
+//    function link(scope, element, attrs){
+//
+//        var ani = 0
+//        element.bind('mousewheel', function(e){
+//            if(e.originalEvent.wheelDelta > 0 ){
+//                ani = 0;
+//                if(ani == 1)
+//                    element.stop()
+//                element.animate({
+//                    scrollTop: $(this).scrollTop() - 200
+//                }, 1000 );
+//                console.log('up');
+//            }else {
+//                ani = 1;
+//                if(ani == 0)
+//                    element.stop();
+//                element.animate({
+//                    scrollTop: $(this).scrollTop() + 200
+//                }, 1000 );
+//                console.log('down');
+//            }
+//
+//        })
+//
+//    }
+//
+//    return{
+//        restrict: 'A',
+//        link: link
+//    }
+//
+//
+//})
+
